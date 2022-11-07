a16f68b978be32835e12da49d090eaf129190948
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-11-07 10:54:39 +0500

    Add files via upload

---
+++ Amoliyot1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+string()
+This is a temporary script file.
+Lazizbek Isaqjonov tomonidan Anvar Narzullayev masalariga yechim
+"""
+
+#1.0 "Nexia", "Tico", 'Damas' ko'rganlar qilar havas --> Ekranga chiqaring
+
+#print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+
+#1.1     5 ning 4-darajasini toping
+
+#print("5 ning 4- darajasi", 5**4, "ga teng")
+
+#1.3   22 ni 4 ga bo'lganda qoldiqni toping
+
+#print("22 ni 4 ga bo'lsak qoldiq", 22%4, "ga teng")  
+
+#1.4   Tomolari 125 ga teng kvadratning yuzi va peremetrini yoping
+
+#print("tomonlari 125 ga teng bo'lgan kvadratning yuzi", 125**2, "ga teng peremtri esa\n",
+#      125*4, "ga teng")
+
+#1.5    Diametri 12 ga teng bo'lgan doiraning yuzini toping
+
+#pi=3.14159
+#d=12
+#r=d/2
+#s=pi*r**2
+#print("doiraning yuzi", s, "ga teng")
+
+#1.6   Katetlari 6 va 7 ga teng bo'lgan uchburchakning gipotenuzasini toping
+
+#a=6
+#b=7
+#c=(a*a+b**2)**0.5
+#print("uchburchak gipotenuzasi", c, "ga teng")
+
