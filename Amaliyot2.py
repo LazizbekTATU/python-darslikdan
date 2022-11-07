73c8deeb010373a3b10657594d237a211c5dbc48
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-11-07 11:28:25 +0500

    Add files via upload

---
+++ Amaliyot2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 10:46:40 2022
+Lazizbek Isaqjonov
+@author: user
+"""
+
+#2.0  "Hello world!" matnini yangi o'zgaruvchiga yuklang va print() 
+#funksiyasi yordamida consulga chiqaring
+
+#salom = "Hello world "
+#print(salom) 
+
+#2.1   xabar deb nomlangan o'zgaruvchiga yangi qiymat bering va konsolga
+# chiqaring
+
+#class = "class dan foydalana olmaymiz"
+#print(class) 
+
+# class dan o'zgaruvchi sifatida foydalana olmaymiz esdan chiqarmang, umuman 
+# olganda dasturlashda funksiyalarni o'zgaruvchi sifatida qo'llay olmaymiz
+
+#2.2   radiusi 5 ga teng doira yuzini toping
+
+#radius = 5
+#pi = 3.14159
+#yuza = pi*radius**2
+#print("Radiusi", radius, "ga teng bo'lgan doiraning yuzi=", yuza)
