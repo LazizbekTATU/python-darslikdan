daf1b011230064a1433c6398c43a887448917d7a
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-11-07 12:47:54 +0500

    Add files via upload

---
+++ Amaliyot3.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 12:13:31 2022
+Amaliyot3
+
+Isaqjonov Lazizbek
+
+@author: user
+"""
+#3.0 o'zgaruvchilarni yarating
+
+#kocha="Bog'bon"
+#mahalla="Sog'bon"
+#tuman="Bodomzor" 
+#viloyat="Samarqand"
+
+#print(kocha, "ko'chasi", mahalla, "mahallasi", tuman, "tumani", viloyat, "viloyati")
+
+#3.2 
+
+#kocha = str(input("kochani kiriting: "))
+#mahalla = str(input("mahallani kiriting:"))
+#tuman = str(input("tumanni kiriting:"))
+#viloyat = str(input("viloyatni kiriting:"))
+
+#print(kocha, "ko'chasi", mahalla, "mahallasi", tuman, "tumani", viloyat, "viloyati")
+
+#3.3    
+
+#kocha = str(input("kochani kiriting: "))
+#mahalla = str(input("mahallani kiriting:"))
+#tuman = str(input("tumanni kiriting:"))
+#viloyat = str(input("viloyatni kiriting:"))
+
+#print(kocha, "ko'chasi\n", mahalla, "mahallasi\n", tuman, "tumani\n", viloyat, "viloyati\n")
+
+
+#3.4
+
+#kocha = str(input("kochani kiriting: "))
+#mahalla = str(input("mahallani kiriting:"))
+#tuman = str(input("tumanni kiriting:"))
+#viloyat = str(input("viloyatni kiriting:"))
+
+#print(f"{kocha} kochasi, {mahalla} mahallasi , {tuman} tumani,{viloyat} viloyati")
+
+
+
+#3.5 
+
+
+#kocha = str(input("kochani kiriting: "))
+#mahalla = str(input("mahallani kiriting:"))
+#tuman = str(input("tumanni kiriting:"))
+#viloyat = str(input("viloyatni kiriting:"))
+
+#print(f"{kocha} kochasi, {mahalla} mahallasi , {tuman} tumani,{viloyat} viloyati".lower())
+#print(f"{kocha} kochasi, {mahalla} mahallasi , {tuman} tumani,{viloyat} viloyati".upper())
+#print(f"{kocha} kochasi, {mahalla} mahallasi , {tuman} tumani,{viloyat} viloyati".capitalize())
+#print(f"{kocha} kochasi, {mahalla} mahallasi , {tuman} tumani,{viloyat} viloyati".title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
