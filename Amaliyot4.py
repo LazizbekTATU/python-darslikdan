e862c134c77eabcb5532db03220ca7846d6f4448
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-11-10 11:57:31 +0500

    Add files via upload

---
+++ Amaliyot4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:48:55 2022
+
+Isaqjonov Lazizbek
+
+@author: user
+"""
+#1   Foydalanuvchi kiritgan sonning kvadrati va kubini chiqaring
+#a = int(input("a = "))
+
+#print(a**2, a**3)
+
+
+#2 Foydalanuvchi yoshini so'rab, uning tug'ilgan yiini hisoblab, konsulga
+# chiqaruvchi dastur
+
+#yosh = input("Yoshingizni kiriting:")
+
+#yosh = int(yosh)
+
+#t_yil = 2022-yosh
+
+#print("Siz", t_yil,"da tug'ilgansiz")
+
+
+
+#3 Foydalanuvchidan ikki son kiritishini so'rab, kiritilgan sonlarning
+#yig'indisi, ayirmasi, ko'paytmasi, va bo'linmasini chiqaruvchi dastur
+
+#a = int(input("a = "))
+#b = int(input("b = "))
+
+#print(a+b, a-b,a*b,a/b)
