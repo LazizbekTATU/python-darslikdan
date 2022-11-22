88368eca83435fc2af3f2f85a6600a360424ac20
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-11-22 09:52:05 +0500

    Add files via upload

---
+++ Amaliyot6.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 09:10:31 2022
+
+@author: user
+"""
+#1.00
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#print(davlatlar)
+
+#1.01 ro'yhat uzunligini chiqaring
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#print("ro'yhatdagi davlatlar uzunligi",len(davlatlar),"ga teng")
+
+
+#1.02
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#print(sorted(davlatlar))
+
+#1.03
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#print(davlatlar)
+
+#1.04
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#davlatlar.reverse()
+#
+
+#1.05
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#davlatlar.sort()
+#print(davlatlar)
+
+#1.06
+
+#davlatlar = ["Eron", "Rossiya", "Uzbekistan","AQSH","Yaponiya","Turkiya"]
+#davlatlar.sort(reverse=True)
+#print(davlatlar)
+
+
+#2.00
+
+#sonlar=list(range(120,1201,1))
+#print(sonlar)
+
+
+#2.01
+
+#sonlar=list(range(120,1201,1))
+#print(sum(sonlar))
+
+#2.02
+
+#sonlar=list(range(120,1201,1))
+#print(max(sonlar)-min(sonlar))
+
+
+#2.03
+
+#sonlar=list(range(120,1201,1))
+#print(len(sonlar))
+
+#2.04
+
+#sonlar=list(range(120,1201,1))
+#print(sonlar[:20])
+#print(sonlar[530:550])
+#print(sonlar[-20:])
+
+#2.05
+
+taomlar=["manti",'osh','shashlik',"lagmon","bosma"]
+nonushta=taomlar[:]
+nonushta.append("shorva")
+nonushta.append("mastava")
+print(taomlar)
+print(nonushta)
+
+#2.06
+nonushta = tuple(nonushta)
+nonushta[0]="qaymoq va non"
+
+
+
+
+
+
+
+
+
+
+
