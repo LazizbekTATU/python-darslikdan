dada1aa3844015924afaeb4f8888e5556a47ebf5
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2022-12-01 16:01:20 +0500

    Add files via upload

---
+++ Amaliyot8.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 12:09:56 2022
+
+@author: user
+"""
+
+#avtolar = ["audi", "bmw",'volvo','kia','hyundai']
+#for avto in avtolar:
+#    if avto == 'bmw':
+#        print(avto.upper())
+#    else:
+#        print(avto.title())
+ 
+
+#yil = int(input("Tug'ulgan yilingizni kiriting:"))
+#if 2021-yil<18:
+#    print(f"Yoshingiz {2021-yil}da ekan")
+#    print("kiriah mumkin emas")
+#else:
+#    print("Xush kelibsiz")
+
+#1.08
+
+#cars = ['toyota', 'mazda','hyundai','gm','kia']
+
+#for avto in cars:
+#    if avto=="gm":
+#       print(avto.upper())
+#    else:
+#        print(avto.title())
+
+#2.08
+#cars = ['toyota','mazda','hyundayi','gm','kia']
+
+#for avto in cars:
+#    if avto!='gm':
+#        print(avto.title())
+#    else:
+#        print(avto.upper())
+
+#3.08
+
+#login =(input("login ismini kiriting:"))
+#if login.title()=="Admin":
+    
+#    print(f"Xush kelibsiz, {login.title()}. Foydalanuvchilar royhatini korasizmi")
+#else:
+#    print(f"Xush kelibsiz, {login.title()}")  
+
+
+#3.09
+
+#a = int(input("a = "))
+#b = int(input("b = "))
+
+#if a==b:
+#    print("Sonlar teng")
+#else:
+#    print("Teng emas")
+
+#4.08
+
+#a = int(input("a = "))
+#if a>0:
+#    print("Musbat son")
+#if a<0:
+#    print("Manfiy son")
+
+#5.08
+
+#a = int(input("a = "))
+#if a>0:
+#    print(a**0.5)
+#if a<0:
+#    print("Musbat son kiriting")
+
+#6.08
+
+#a = int(input("a = "))
+
+#if a%2==0:
+#    print("Juft son")
+#if a%2==1:
+#    print("Toq son")
+
+
+
